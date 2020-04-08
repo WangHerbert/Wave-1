c7039cfc5824cb1eb969fa98d27b5cfd4a83bc7f
Herbert Wang <herbertwang@Honda-Civic.local>
2020-04-08 14:38:46 -0400

    Expanded the code to make it more legible

--- VolumeofCylinder.py
+++ VolumeofCylinder.py
@@ -1,8 +1,12 @@
 import math
 
-radius = float(input('Enter the radius of the cylinder: '))
-height = float(input('Enter the height of the cylinder: '))
+radius = (input('Enter the radius of the cylinder: '))
+height = (input('Enter the height of the cylinder: '))
+
+radius = float(radius)
+height = float(radius)
 
 volume = (radius ** 2 * math.pi) * height
+volume = round(volume, 1)
 
-print ('The volume of the cylinder is ' + str(round(volume,1)) + ' units squared')
+print ('The volume of the cylinder is ' + str(volume) + ' units squared')
